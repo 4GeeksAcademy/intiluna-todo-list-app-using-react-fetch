03f5bd9571656f2fb387c2619bfd5bedfea251c9
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-06-26 11:12:47 +0000

    agrega funcionalidad de usar input, cambia array y elimina elemento de array

--- src/js/component/form.jsx
+++ src/js/component/form.jsx
@@ -1,32 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import "../../styles/index.css"
 
 const Form = () => {
     
-    //const [tareas, setTareas] = useState(["comer", "correr","nadar","cocinar"])
-    const tareas = ["comer", "correr","nadar","cocinar"]
+    const [inputValue, setInputValue ] = useState('');
+   
+    const [tareas, setTareas] = useState(["comer", "correr","nadar","cocinar"])
+    //const tareas = ["comer", "correr","nadar","cocinar"]
 
-    function actualizarTareas(){
-        setTareas(tareas.concat(item))
+    function actualizarTareas(e){
+        if ((e.key === "1") && (inputValue!="")){
+            setTareas((datosPrevios) => [...datosPrevios, inputValue]);
+        }
+        
     }
     
     function eliminarTarea(item) {
         console.log("funciona " + item);
         //filtrar
-        tareas.filter((elemento)=>elemento!=item)
-        console.log("tareas: " + tareas );
+        const nuevaLista = tareas.filter((elemento)=>elemento!=item)
+        setTareas(nuevaLista)
+        //console.log("tareas: " + tareas );
     }
     
+    //
+    // <input type="text" onChange={e => setInputValue(e.target.value)} value={inputValue} />
+
     return (
         <div>
             <div className="inputTarea">
-            <input className="form-control" type="text" placeholder="What needs to be done?" aria-label="default input example"/>
+            <input className="form-control" type="text" onChange={e => setInputValue(e.target.value)} value={inputValue} onKeyDown={(e)=>actualizarTareas(e)} placeholder="What needs to be done?" aria-label="default input example"/>      
+            
+            
         </div>
         
         <div className="ListaTareas">
         <div className="display">My TODO List</div>
             <ul>
-                {tareas.map((item)=><li>{item} <span onClick={()=>eliminarTarea(item)}>x</span></li>)}
+                {tareas.map((item,index)=><li key={index}>{item} <span onClick={()=>eliminarTarea(item)}>x</span> </li>)}
             </ul>
         </div>
 
